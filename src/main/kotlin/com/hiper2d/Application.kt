ba34c39cf2bce3117c71c7c1c5af6d233397585e
Alexey Zelenovsky <hiper2d@gmail.com>
2018-12-11 23:21:46 -0500

    Experimented with WebSockets. Didn't manage to make them send messages to all clients

--- src/main/kotlin/com/hiper2d/Application.kt
+++ src/main/kotlin/com/hiper2d/Application.kt
@@ -8,6 +8,7 @@ import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.reactive.HandlerMapping
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
+import org.springframework.web.reactive.socket.WebSocketSession
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
 
 
@@ -18,18 +19,34 @@ class Application {
     class WebSocketConfig {
 
         @Bean
-        fun webSocketHandlerAdapter(): WebSocketHandlerAdapter {
-            return WebSocketHandlerAdapter()
-        }
+        fun webSocketHandlerAdapter() = WebSocketHandlerAdapter()
 
         @Bean
-        fun echoWebSocketHandler() =
-            WebSocketHandler { session ->
-                val publisher = session.receive().doOnNext {
-                    println(it.payloadAsText)
-                }
-                session.send(publisher).doFinally { println("Disconnected") }
+        fun echoWebSocketHandler(): WebSocketHandler {
+            val sessions = mutableSetOf<WebSocketSession>()
+
+            return WebSocketHandler { session ->
+                val sessionId = session.id
+                val output = session.receive()
+                    .doOnSubscribe {
+                        println("Session $sessionId has been subscribed")
+                        sessions.add(session)
+                    }
+                    .map { msg ->
+                        var tmp = msg
+                        sessions.forEach {
+                            tmp = it.textMessage(it.id)
+                        }
+                        tmp
+                    }
+                    .doFinally {
+                        sessions.remove(session)
+                        println("Session $sessionId has been terminated")
+                    }
+
+                session.send(output).doFinally { println("Output Stream Disconnected") }
             }
+        }
 
         @Bean
         fun webSocketURLMapping(): HandlerMapping {
